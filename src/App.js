9a476288047a9d33ea52c1a1f0fd992663a9f2af
krapas170 <krapas170@gmail.com>
2023-03-30 23:24:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Forks from "./pages/Forks";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Header />
-      {/* Hier kommt der restliche Inhalt */}
+      <div id="display">
+        <About />
+        <Projects />
+        <Forks />
+        <Blog />
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
