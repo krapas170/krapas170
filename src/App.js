ecb23bd4cddad2b6a62deec03273757da81b784a
krapas170 <krapas170@gmail.com>
2023-03-30 18:02:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { useEffect } from "react";
 import Header from "./components/Header";
-import { initGoogleAnalytics } from "./utils/gtag";
 
 function App() {
-  useEffect(() => {
-    initGoogleAnalytics();
-  }, []);
-
   return (
     <div className="App">
       <Header />
