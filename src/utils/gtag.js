92ad344450549841c22801d1f6bd493d4ba9b115
krapas170 <krapas170@gmail.com>
2023-03-30 23:27:16 +0200

    Delete gtag.js

--- src/utils/gtag.js
+++
@@ -1,8 +0,0 @@
-export const dataLayer = window.dataLayer || [];
-
-export function initGoogleAnalytics() {
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BX8JKCVJYX');
-}
