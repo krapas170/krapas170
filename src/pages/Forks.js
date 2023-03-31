9310c62ad7900b8bce986681285b54114ae423f2
krapas170 <krapas170@gmail.com>
2023-03-31 22:51:29 +0200

    Update Forks.js

--- src/pages/Forks.js
+++ src/pages/Forks.js
@@ -1,60 +1,55 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-grid-system";
 
 function Forks() {
+  const [forks, setForks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/krapas170/repos")
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredForks = data.filter((repo) => repo.fork === true);
+        const sortedForks = filteredForks.slice(0, 4);
+        setForks(sortedForks);
+      });
+  }, []);
+
   return (
     <Container id="forks">
       <h1>Forks</h1>
-      <div className="projects" id="forks_section">
-        <a
-          href="https://github.com/krapas170/OldJavaTutorial"
-          target="_blank"
-          style={{ position: "relative" }}
-        >
-          <section>
-            <div className="section_title">OldJavaTutorial</div>
-            <div className="about_section">
-              <span style={{ display: "none" }}>undefined</span>
-            </div>
-            <div className="bottom_section">
-              <span style={{ display: "none" }}>
-                <i className="fas fa-code"></i>&nbsp; null
-              </span>
-              <span>
-                <i className="fas fa-star"></i>&nbsp; 0
-              </span>
-              <span>
-                <i className="fas fa-code-branch"></i>&nbsp; 0
-              </span>
-            </div>
-          </section>
-        </a>
-        <a
-          href="https://github.com/krapas170/Github-Webhook"
-          target="_blank"
-          style={{ position: "relative" }}
-        >
-          <section>
-            <div className="section_title">Github-Webhook</div>
-            <div className="about_section">
-              <span style={{ display: "block" }}>
-                Simple Api to handle GitHub webhooks and redirect them beautiful
-                to Discord
-              </span>
-            </div>
-            <div className="bottom_section">
-              <span style={{ display: "none" }}>
-                <i className="fas fa-code"></i>&nbsp; null
-              </span>
-              <span>
-                <i className="fas fa-star"></i>&nbsp; 0
-              </span>
-              <span>
-                <i className="fas fa-code-branch"></i>&nbsp; 0
-              </span>
-            </div>
-          </section>
-        </a>
+      <p>My last four forked repositories on GitHub</p>
+      <div
+        className="projects"
+        id="forks_section"
+        style={{ position: "relative" }}
+      >
+        <Row>
+          {forks.map((fork) => (
+            <Col key={fork.id} sm={6}>
+              <a href={fork.html_url} target="_blank" rel="noopener noreferrer">
+                <section>
+                  <div className="section_title">{fork.name}</div>
+                  <div className="about_section">
+                    <span style={{ display: "block" }}>{fork.description}</span>
+                  </div>
+                  <div className="bottom_section">
+                    <span style={{ display: "inline-block" }}>
+                      <i className="fas fa-code"></i>&nbsp;
+                      {fork.language || "Unknown"}
+                    </span>
+                    <span>
+                      <i className="fas fa-star"></i>&nbsp;{fork.forks}
+                    </span>
+                    <span>
+                      <i className="fas fa-code-branch"></i>&nbsp;
+                      {fork.stargazers_count}
+                    </span>
+                  </div>
+                </section>
+              </a>
+            </Col>
+          ))}
+        </Row>
       </div>
     </Container>
   );
