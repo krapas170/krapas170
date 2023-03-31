85e662022fbe2b2f9353f585961f8c6903f3eedf
krapas170 <krapas170@gmail.com>
2023-03-31 22:49:29 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
 
 function Header() {
+  // eslint-disable-next-line
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#work">Work</Nav.Link>
+              <Nav.Link href="#forks">Forks</Nav.Link>
+              <Nav.Link href="#blog">Blog</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 }
