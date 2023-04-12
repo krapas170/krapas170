de3c73297c8d51775c86783df48aa95a22043ca4
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-04-12 11:25:32 +0000

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,85 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-scroll";
+import { useLocation } from "react-router-dom";
+
 import "./Header.css";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#work">Work</Nav.Link>
-              <Nav.Link href="#forks">Forks</Nav.Link>
-              <Nav.Link href="#blog">Blog</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#home" ? "active" : ""
+                }`}
+              >
+                Home
+              </Link>
+              <Link
+                to="about"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#about" ? "active" : ""
+                }`}
+              >
+                About
+              </Link>
+              <Link
+                to="work"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#work" ? "active" : ""
+                }`}
+              >
+                Work
+              </Link>
+              <Link
+                to="forks"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#forks" ? "active" : ""
+                }`}
+              >
+                Forks
+              </Link>
+              <Link
+                to="blog"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#blog" ? "active" : ""
+                }`}
+              >
+                Blog
+              </Link>
+              <Link
+                to="contact"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className={`nav-link ${
+                  location.hash === "#contact" ? "active" : ""
+                }`}
+              >
+                Contact
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
