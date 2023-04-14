be4e3298060eb928c8f399a77dc459892dd5f8e7
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-04-14 10:48:56 +0000

    update susites

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Forks from "./pages/Forks";
 import Projects from "./pages/Projects";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Header />
-      <div id="display">
-        <About />
-        <Projects />
-        <Forks />
-        <Blog />
-        <Contact />
+    <Router>
+      <div className="App">
+        <Sidebar />
+        <Header />
+        <TransitionGroup>
+          <CSSTransition timeout={500} classNames="fade" key={location.key}>
+            <div id="display">
+              <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/work" element={<Projects />} />
+                <Route path="/forks" element={<Forks />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </div>
+          </CSSTransition>
+        </TransitionGroup>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,89 +1,17 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-scroll";
-import { useLocation } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  const location = useLocation();
-
   return (
     <header>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link
-                to="home"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#home" ? "active" : ""
-                }`}
-              >
-                Home
-              </Link>
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#about" ? "active" : ""
-                }`}
-              >
-                About
-              </Link>
-              <Link
-                to="work"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#work" ? "active" : ""
-                }`}
-              >
-                Work
-              </Link>
-              <Link
-                to="forks"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#forks" ? "active" : ""
-                }`}
-              >
-                Forks
-              </Link>
-              <Link
-                to="blog"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#blog" ? "active" : ""
-                }`}
-              >
-                Blog
-              </Link>
-              <Link
-                to="contact"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={`nav-link ${
-                  location.hash === "#contact" ? "active" : ""
-                }`}
-              >
-                Contact
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <nav>
+        <Link to="/work">Work</Link>
+        <Link to="/about">About</Link>
+        <Link to="/forks">Forks</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
     </header>
   );
 }
