e658e991e7914befb1b6294433449998d7bd24db
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-04-14 17:04:05 +0000

    feat: Add loading and error handling for data retrieval process
    The new implementation adds the state and methods necessary to show a loading spinner and handle data retrieval errors. In case the request takes over 30s, an error message pops up. In turn, a reload button is provided for the user.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
@@ -11,14 +11,58 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [loadTimeout, setLoadTimeout] = useState(null);
+  const [loadError, setLoadError] = useState(false);
+
+  const handleLoadStart = () => {
+    setLoading(true);
+    setLoadTimeout(
+      setTimeout(() => {
+        setLoadError(true);
+      }, 30000)
+    );
+  };
+
+  const handleLoadEnd = () => {
+    setLoading(false);
+    setLoadError(false);
+    clearTimeout(loadTimeout);
+  };
+
+  const handleLoadError = () => {
+    setLoadError(true);
+  };
+
+  const handleReload = () => {
+    window.location.reload(true);
+  };
+
   return (
     <Router>
       <div className="App">
         <Sidebar />
         <Header />
         <TransitionGroup>
-          <CSSTransition timeout={500} classNames="fade" /*key={location.key}*/>
+          <CSSTransition
+            timeout={500}
+            classNames="fade"
+            onEnter={handleLoadStart}
+            onEntered={handleLoadEnd}
+            onExited={handleLoadEnd}
+          >
             <div id="display">
+              {loading && (
+                <div className="loader">
+                  {!loadError && <div className="loader-circle"></div>}
+                  {loadError && (
+                    <div className="loader-error">
+                      <span>Anscheinend dauert es etwas länger zum Laden</span>
+                      <button onClick={handleReload}>Ja</button>
+                    </div>
+                  )}
+                </div>
+              )}
               <Routes>
                 <Route path="/" element={<Projects />} />
                 <Route path="/about" element={<About />} />
